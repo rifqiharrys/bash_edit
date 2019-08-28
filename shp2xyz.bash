a9f6f9f174466ecd0ecc576febee5d1a2b66c3f4
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-28 09:56:03 +0700

    add filter

--- shp2xyz.bash
+++ shp2xyz.bash
@@ -10,7 +10,7 @@ for shpname in `cat fileshp.txt`; do
 	ogrinfo -al ${shpname}.shp | grep "NIKDLM" | sed -e 's/  NIKDLM (Real) = //g'  > z.txt
 	
 	## garis pantai
-	ogrinfo -al ${shpname}.shp | grep "ELEVAS" | sed -e 's/  ELEVAS (Real) = //g'  >> z.txt
+	ogrinfo -al ${shpname}.shp | grep "ELEVA" | sed -e 's/  ELEVAS (Real) = //g' -e 's/  ELEVATION (Real) = //g' >> z.txt
 	
 	paste -d" " xy_2.txt z.txt > ${shpname}.txt
 	rm xy.txt xy_2.txt z.txt
