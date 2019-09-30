3b88e012b4d2342336af1422bcefec451d2338b5
rifqiharrys <rifqi.m.h@gmail.com>
2019-09-30 10:53:15 +0700

    del ../

--- gdb2shp.bash
+++ gdb2shp.bash
@@ -2,5 +2,5 @@
 
 ls -d *.gdb | sed -e 's/.gdb//g'| sed -e 's/\.\.//g' | sed -e 's/\///g' > filegdb.txt
 for gdbname in `cat filegdb.txt`; do
-	ogr2ogr -f "ESRI shapefile" ${gdbname} ../${gdbname}.gdb
+	ogr2ogr -f "ESRI shapefile" ${gdbname} ${gdbname}.gdb
 done
