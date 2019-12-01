938abe02086dcda1efb60831e6f0fc438221052b
rifqiharrys <rifqi.m.h@gmail.com>
2019-12-01 19:39:30 +0700

    add time stamp recording

---
+++ timediff.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+t0=`timedatectl | grep "Local time" | sed -e 's/                      Local time: //g'`
+
+$1
+
+t1=`timedatectl | grep "Local time" | sed -e 's/                      Local time: //g'`
+
+echo $t0 - $t1
